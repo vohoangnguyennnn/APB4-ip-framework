75e80408a853e9a66baae06a2aa42165ed43b43a
vohoangnguyennnn <251036104+nguyenvohoanggggg@users.noreply.github.com>
2026-07-06 12:22:05 +0700

    feat(apb): add APB4 PROT signal enums and bit-position constants

--- rtl/common/apb_pkg.sv
+++ rtl/common/apb_pkg.sv
@@ -6,14 +6,33 @@ package apb_pkg;
     APB_ACCESS = 2'b10
   } apb_state_e;
 
-  typedef enum logic [1:0] {
-    APB_WRITE = 2'b00,
-    APB_READ = 2'b01
+  typedef enum logic {
+    APB_READ = 1'b0,
+    APB_WRITE = 1'b1
   } apb_dir_e;
 
-  typedef enum logic [1:0] {
-    APB_OKAY = 2'b00,
-    APB_ERROR = 2'b01
+  typedef enum logic {
+    APB_OKAY = 1'b0,
+    APB_ERROR = 1'b1
   } apb_resp_e;
+
+  typedef enum logic {
+    APB_NORMAL = 1'b0,
+    APB_PRIVILEGED = 1'b1
+  } apb_priv_e;
+
+  typedef enum logic {
+    APB_DATA = 1'b0,
+    APB_INSTRUCTION = 1'b1
+  } apb_access_e;
+
+  typedef enum logic {
+    APB_SECURE = 1'b0,
+    APB_NONSECURE = 1'b1
+  } apb_sec_e;
+
+  localparam int APB_PPROT_PRIV_BIT = 0;
+  localparam int APB_PPROT_SEC_BIT = 1;
+  localparam int APB_PPROT_ACCESS_BIT = 2;
 
 endpackage : apb_pkg
