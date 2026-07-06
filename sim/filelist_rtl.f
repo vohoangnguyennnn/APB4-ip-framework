bd48879eb89b6c1f384b94df30862796b2e30d77
vohoangnguyennnn <251036104+nguyenvohoanggggg@users.noreply.github.com>
2026-07-06 18:40:36 +0700

    add simulation filelists

---
+++ sim/filelist_rtl.f
@@ -0,0 +1,12 @@
++incdir+rtl/common
++incdir+rtl/gpio
++incdir+rtl/timer
+
+rtl/common/apb_pkg.sv
+rtl/common/apb_if.sv
+rtl/common/apb_slave_ctrl.sv
+rtl/common/generic_regs.sv
+rtl/gpio/gpio_core.sv
+rtl/gpio/apb_gpio.sv
+rtl/timer/timer_core.sv
+rtl/timer/apb_timer.sv
---
+++ sim/filelist_tb.f
@@ -0,0 +1,1 @@
+-f sim/filelist_rtl.f
